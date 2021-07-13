0b8523722b2d1192cdcfa03f46bbc3e0d6cedaf7
billgotz <vgotzagiannis@gmail.com>
2021-07-13 19:37:11 +0300

    updated links and added email

--- src/components/socials.tsx
+++ src/components/socials.tsx
@@ -5,15 +5,17 @@ import { FaInstagram } from "@react-icons/all-files/fa/FaInstagram"
 import { FaBehance } from "@react-icons/all-files/fa/FaBehance"
 import { FaDribbble } from "@react-icons/all-files/fa/FaDribbble"
 import { FaVimeoV } from "@react-icons/all-files/fa/FaVimeoV"
+import { FaLink } from "@react-icons/all-files/fa/FaLink"
 
 export default function Socials() {
-  const links = [
-    // { icon: <FaFacebookF />, href: "https://www.instagram.com/dio.gotz/" },
-    // { icon: <FaFacebookF />, href: "https://www.instagram.com/dio.gotz/" },
-  ]
-
   return (
     <>
+      <li>
+        <a href="mailto:gkotzagiannhs.dion.le@gmail.com">
+          <FaLink />
+        </a>
+      </li>
+
       <li>
         <a href="https://www.instagram.com/dio.gotz/">
           <FaInstagram />
@@ -21,25 +23,25 @@ export default function Socials() {
       </li>
 
       <li>
-        <a href="https://www.instagram.com/dio.gotz/">
+        <a href="https://www.linkedin.com/in/dionisis-gotzagiannis/">
           <FaLinkedinIn />
         </a>
       </li>
 
       <li>
-        <a href="https://www.instagram.com/dio.gotz/">
+        <a href="https://www.behance.net/dio-G">
           <FaBehance />
         </a>
       </li>
 
       <li>
-        <a href="https://www.instagram.com/dio.gotz/">
+        <a href="https://dribbble.com/_Dee">
           <FaDribbble />
         </a>
       </li>
 
       <li>
-        <a href="https://www.instagram.com/dio.gotz/">
+        <a href="https://vimeo.com/user53571977">
           <FaVimeoV />
         </a>
       </li>
