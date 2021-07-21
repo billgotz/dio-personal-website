485776ef1ad5d7d9a65b539d98463c2ced371be0
billgotz <vgotzagiannis@gmail.com>
2021-07-21 21:00:41 +0300

    added some copyright :)

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -43,6 +43,15 @@ function Layout({ children }) {
 
       <main data-scroll>{children}</main>
       <Footer />
+
+      <p className="text-center pb-5 whitespace-pre-wrap">
+        Copyright © 2021
+        {"\n"}
+        <small>
+          Website created with ❤️ by{" "}
+          <a href="https://github.com/billgotz">Vasilis Gkotzagiannis </a>
+        </small>
+      </p>
     </div>
   )
 }
